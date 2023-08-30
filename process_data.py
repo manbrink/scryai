d4dab3989816dc7cc257570e4052bd142fd99f06
Mike Brinkman <mbrinkma13@gmail.com>
2023-08-29 20:56:35 -0400

    remove duplicate names

--- process_data.py
+++ process_data.py
@@ -5,16 +5,25 @@ import pandas as pd
 with open('data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-# Prepare an empty dictionary for holding data
+# Initialize dictionary to hold the data
 data_dict = {'id': [], 'cmc': []}
 for col in ['W', 'U', 'B', 'R', 'G']:
     data_dict[col] = []
 
-# Set of all unique keywords across all records
-all_keywords = set()
+# Set for keeping track of unique processed names
+processed_names = set()
 
-# Iterate over records to populate color and keyword columns
+# Loop over each record in the JSON data
 for record in data:
+    # Get the name and skip this iteration if the name has already been processed
+    name = record.get('name', '')
+    if name in processed_names:
+        continue
+
+    # Mark this name as processed
+    processed_names.add(name)
+
+    # Append basic data
     data_dict['id'].append(record.get('id', ''))
     data_dict['cmc'].append(record.get('cmc', 0))
 
@@ -22,38 +31,29 @@ for record in data:
     for col in ['W', 'U', 'B', 'R', 'G']:
         data_dict[col].append(0)
 
+    # Update colors if present
     colors = record.get('colors', [])
-    if isinstance(colors, list):
-        for col in colors:
-            if col in ['W', 'U', 'B', 'R', 'G']:
-                data_dict[col][-1] = 1  # set the last entry to 1
-    
-    # Update the set of all unique keywords
+    for col in colors:
+        if col in ['W', 'U', 'B', 'R', 'G']:
+            data_dict[col][-1] = 1
+
+    # Add any new keywords found
     keywords = record.get('keywords', [])
-    if isinstance(keywords, list):
-        all_keywords.update(keywords)
+    for keyword in keywords:
+        if keyword not in data_dict:
+            data_dict[keyword] = [0] * len(data_dict['id'])  # Initialize with zeros
+        data_dict[keyword][-1] = 1  # Set the last element to 1
 
-# Add keyword columns to dictionary and initialize them to 0
-for keyword in all_keywords:
-    data_dict[keyword] = [0] * len(data)
+# Make sure all lists in data_dict have the same length
+for key, value in data_dict.items():
+    if len(value) < len(data_dict['id']):
+        data_dict[key].extend([0] * (len(data_dict['id']) - len(value)))
 
-# Iterate again to populate keyword columns
-for i, record in enumerate(data):
-    keywords = record.get('keywords', [])
-    if isinstance(keywords, list):
-        for keyword in keywords:
-            if keyword in all_keywords:
-                data_dict[keyword][i] = 1
-
-# Convert the dictionary to a DataFrame
+# Convert to DataFrame
 df_filtered = pd.DataFrame(data_dict)
 
-# Convert 'cmc' to integers
+# Clean up the 'cmc' column
 df_filtered['cmc'] = df_filtered['cmc'].fillna(0).astype(int)
 
+# Write to CSV
 df_filtered.to_csv('processed_data.csv', index=False)
-
-# Show the first 10 rows
-# print(df_filtered.head(10))
-
-
--- train_model.py
+++ train_model.py
@@ -1,24 +1,43 @@
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
+import json
 
+# Load the JSON data
+with open('data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+# Assume df_filtered is already created and saved
 df_filtered = pd.read_csv('processed_data.csv')
 
+# Extract feature matrix and id_index
 X = df_filtered.drop('id', axis=1).to_numpy()
 id_index = df_filtered['id'].to_list()
 
-k = 5  # number of neighbors
+# Create a dictionary to map 'id' to 'name' from the original JSON data
+id_to_name = {record.get('id', ''): record.get('name', '') for record in data}
+
+# Initialize k-NN model
+k = 15  # number of neighbors
 knn = KNeighborsClassifier(n_neighbors=k)
 
+# Train the model
 knn.fit(X, id_index)
 
+# Function to find nearest neighbors
 def find_nearest_neighbors(record_id, id_index, knn_model, X):
     record_idx = id_index.index(record_id)
     distances, indices = knn_model.kneighbors(X[record_idx].reshape(1, -1))
     neighbors = [id_index[i] for i in indices[0]]
     return neighbors
 
-# Sample query
-record_id = 'b81c6c8b-a9cf-4866-89ba-7f8ad077b836'
-nearest_neighbors = find_nearest_neighbors(record_id, id_index, knn, X)
-print(f"Nearest neighbors to {record_id} are {nearest_neighbors}")
+# Query example
+record_id = '0000579f-7b35-4ed3-b44c-db2a538066fe'  # Replace with an actual id from your data
+nearest_neighbor_ids = find_nearest_neighbors(record_id, id_index, knn, X)
+
+# Retrieve names for the nearest neighbor ids
+nearest_neighbor_names = [id_to_name[n_id] for n_id in nearest_neighbor_ids]
+
+print(f"Nearest neighbors to ID {record_id} are:")
+for n_id, n_name in zip(nearest_neighbor_ids, nearest_neighbor_names):
+    print(f"ID: {n_id}, Name: {n_name}")
